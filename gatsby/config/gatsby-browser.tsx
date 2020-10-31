0e130c982b73c66ab9f7cd20d66e17afd71a09a9
Andrei Fidelman <andrei.fidelman@ataccama.com>
2020-10-31 18:56:18 +0100

    gatsby(fix) Mock analytics

--- gatsby/config/gatsby-browser.tsx
+++ gatsby/config/gatsby-browser.tsx
@@ -7,19 +7,21 @@ export const wrapPageElement = ({ element }) => {
   return <Layout>{element}</Layout>;
 };
 
-const appInsights = new ApplicationInsights({
-  config: {
-    instrumentationKey: process.env.GATSBY_APPINSIGHTS_KEY,
-  },
-});
 
-export const onInitialClientRender = () => {
-  appInsights.loadAppInsights();
-  appInsights.trackPageView();
-};
+// FIXME: Uncomment when the GATSBY_APPINSIGHTS_KEY is added to Vercel
+// const appInsights = new ApplicationInsights({
+//   config: {
+//     instrumentationKey: process.env.GATSBY_APPINSIGHTS_KEY,
+//   },
+// });
 
-export const onRouteUpdate = () => {
-  appInsights.trackPageView();
-};
+// export const onInitialClientRender = () => {
+//   appInsights.loadAppInsights();
+//   appInsights.trackPageView();
+// };
 
-(window as any).appInsights = appInsights;
+// export const onRouteUpdate = () => {
+//   appInsights.trackPageView();
+// };
+
+// (window as any).appInsights = appInsights;
