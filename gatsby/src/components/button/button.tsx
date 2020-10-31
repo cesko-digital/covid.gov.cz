90b650dc6106599b7c0a009c2042253417eb8fae
Karel Koudelka <k.koudelka01@gmail.com>
2020-10-31 14:47:59 +0000

    feat(button): added property 'disabled'

--- gatsby/src/components/button/button.tsx
+++ gatsby/src/components/button/button.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import classNames from 'classnames';
+
+const getClass = (
+  variant: IProps['variant'],
+  additionalClass?: string,
+): string => {
+  return classNames(
+    'btn',
+    { 'btn-primary': variant === 'contained' },
+    { 'btn-outline-primary': variant === 'outline' },
+    { 'btn-secondary': variant === 'secondary' },
+    { [additionalClass]: additionalClass },
+  );
+};
 interface IProps {
   text: string;
   variant?: 'contained' | 'outline' | 'secondary';
+  additionalClass?: string;
   onClick?: () => void;
   href?: string;
+  disabled?: boolean;
 }
 
 const Button: React.FC<IProps> = ({
@@ -12,32 +27,23 @@ const Button: React.FC<IProps> = ({
   onClick,
   text,
   href = '',
+  additionalClass,
+  disabled = false,
 }) => {
-  if (href !== '') {
+  if (href !== '' && !disabled) {
     return (
-      <a
-        href={href}
-        className={classNames(
-          'btn',
-          { 'btn-primary': variant === 'contained' },
-          { 'btn-outline-primary': variant === 'outline' },
-          { 'btn-secondary': variant === 'secondary' },
-        )}
-      >
+      <a href={href} className={getClass(variant, additionalClass)}>
         {text}
       </a>
     );
   }
+
   return (
     <button
       type="button"
-      className={classNames(
-        'btn',
-        { 'btn-primary': variant === 'contained' },
-        { 'btn-outline-primary': variant === 'outline' },
-        { 'btn-secondary': variant === 'secondary' },
-      )}
+      className={getClass(variant, additionalClass)}
       onClick={onClick || null}
+      disabled={disabled}
     >
       {text}
     </button>
