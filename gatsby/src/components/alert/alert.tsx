f22637f836713bf8a70d18070b2d4baab9255db7
Karel Koudelka <k.koudelka01@gmail.com>
2020-11-02 00:49:44 +0000

    feat(alert): alert container

--- gatsby/src/components/alert/alert.tsx
+++ gatsby/src/components/alert/alert.tsx
@@ -4,47 +4,39 @@ import GovIcon from '../gov-icon';
 import Row from '../row';
 import Col from '../col';
 
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'gatsby-plugin-react-i18next';
 
 interface IProps {
   message: string;
-  startBy: number;
-  endBy?: number;
-  to?: string;
+  link?: string;
 }
 
 const Alert: React.FC<IProps> = ({
   message = 'Probíhá aktualizace dat, prosím, berte na vědomí, že data nemusí být aktuální všude.',
-  startBy = Date.now(),
-  endBy,
-  to,
+  link,
 }) => {
   const { t } = useTranslation();
-  if (
-    (startBy && startBy <= Date.now() && endBy >= Date.now()) ||
-    (startBy && !endBy)
-  )
-    return (
-      <>
-        <Row
-          className="w-100 m-0 bg-warning text-black"
-          justify="center"
-          alignItems="center"
-        >
-          <Col col={11} colMd={6}>
-            <div className="d-flex align-items-center justify-content-center py-1">
-              <GovIcon icon="alert" size={20} />
-              <span className="ml-2">{message}</span>
-              {to != null ? (
-                <Button variant="small-black" text={t('btn_more')} href={to} />
-              ) : (
-                <></>
-              )}
-            </div>
-          </Col>
-        </Row>
-      </>
-    );
+  return (
+    <>
+      <Row
+        className="w-100 m-0 bg-warning text-black"
+        justify="center"
+        alignItems="center"
+      >
+        <Col col={11} colMd={9} colLg={6}>
+          <div className="d-flex align-items-center justify-content-center py-1">
+            <GovIcon icon="alert" size={20} />
+            <span className="ml-2">{message}</span>
+            {link != null ? (
+              <Button variant="small-black" text={t('Více')} href={link} />
+            ) : (
+              <></>
+            )}
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Alert;
