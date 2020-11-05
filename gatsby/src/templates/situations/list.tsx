ac834a6549862529bffd9096981ad92d9ca3b550
David Veselý <dawe.vesely@gmail.com>
2020-11-06 00:34:28 +0100

    Gatsby: unify bradcrumbs add translations

--- gatsby/src/templates/situations/list.tsx
+++ gatsby/src/templates/situations/list.tsx
@@ -5,6 +5,8 @@ import Container from '@/components/container';
 import Headline from '@/components/headline';
 import ListCard from '@/components/list-card';
 import Layout from '@/layouts/default-layout';
+import Breadcrumb from '@/components/breadcrumb';
+import I18n from '@/components/i18n';
 
 interface IProps {
   data: IQuery;
@@ -15,6 +17,19 @@ const Home: React.FC<IProps> = ({ data }) => {
   return (
     <Layout>
       <Container>
+        <div className="pt-1">
+          <Breadcrumb
+            items={[
+              { title: I18n('home'), url: '/' },
+              {
+                title: I18n('life_situations'),
+                url: I18n('slug_situations'),
+              },
+              area.name,
+            ]}
+            variant="inverse"
+          />
+        </div>
         <Headline>{area.name}</Headline>
         <div>
           {area.relationships?.situation?.map(({ id, title, path }) => {
