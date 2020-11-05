29a3f31e6c0a6f32ba0ba32af0297db5df5d8c55
Filip Brebera <breberaf@gmail.com>
2020-11-05 13:23:10 +0100

    fix: fixed snapshot tests

--- gatsby/__tests__/button.react.test.tsx
+++ gatsby/__tests__/button.react.test.tsx
@@ -7,8 +7,8 @@ import { Add } from '@material-ui/icons';
 test('Button renders HTML button element (not anchor element)', () => {
   const component = renderer.create(
     <>
-      <Button />
-      <Button href="" />
+      <Button variant="contained" />
+      <Button href="" variant="contained" />
     </>,
   );
   const tree = component.toJSON();
@@ -16,19 +16,23 @@ test('Button renders HTML button element (not anchor element)', () => {
 });
 
 test('Button with text renders correctly', () => {
-  const component = renderer.create(<Button text="VÍCE" />);
+  const component = renderer.create(<Button text="VÍCE" variant="contained" />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test('Anchor with text inside renders correctly', () => {
-  const component = renderer.create(<Button text="VÍCE" href="#" />);
+  const component = renderer.create(
+    <Button text="VÍCE" href="#" variant="contained" />,
+  );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test('Button with icon renders correctly', () => {
-  const component = renderer.create(<Button icon={<Add />} />);
+  const component = renderer.create(
+    <Button icon={<Add />} variant="contained" />,
+  );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
@@ -54,7 +58,9 @@ test('Button does not render unknown variants classes', () => {
 });
 
 test('Button renders with additional class correctly', () => {
-  const component = renderer.create(<Button className="whatever-button" />);
+  const component = renderer.create(
+    <Button className="whatever-button" variant="contained" />,
+  );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
@@ -84,7 +90,7 @@ test('Anchor does not render unknown variants classes', () => {
 
 test('Anchor renders with additional class correctly', () => {
   const component = renderer.create(
-    <Button className="whatever-anchor" href="#" />,
+    <Button className="whatever-anchor" href="#" variant="contained" />,
   );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
@@ -92,13 +98,15 @@ test('Anchor renders with additional class correctly', () => {
 
 test('Button with onClick renders correctly', () => {
   const toggleSection = () => {};
-  const component = renderer.create(<Button onClick={toggleSection} />);
+  const component = renderer.create(
+    <Button onClick={toggleSection} variant="contained" />,
+  );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test('Anchor element renders correctly', () => {
-  const component = renderer.create(<Button href="#" />);
+  const component = renderer.create(<Button href="#" variant="contained" />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
@@ -106,7 +114,7 @@ test('Anchor element renders correctly', () => {
 test('Button with disabled state renders correctly ', () => {
   const component = renderer.create(
     <>
-      <Button disabled />
+      <Button disabled variant="contained" />
     </>,
   );
   const tree = component.toJSON();
@@ -116,8 +124,8 @@ test('Button with disabled state renders correctly ', () => {
 test('Anchor with disabled state renders like button ', () => {
   const component = renderer.create(
     <>
-      <Button href="#" disabled />
-      <Button href="" disabled />
+      <Button href="#" disabled variant="contained" />
+      <Button href="" disabled variant="contained" />
     </>,
   );
   const tree = component.toJSON();
