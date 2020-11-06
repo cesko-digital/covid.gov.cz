a3c56486a43132a1a88d1e599576eb696c6ca512
Karel Koudelka <k.koudelka01@gmail.com>
2020-11-06 18:12:52 +0000

    fix: menu updated

--- gatsby/src/components/header/header.tsx
+++ gatsby/src/components/header/header.tsx
@@ -38,123 +38,138 @@ const Header: React.FC<Props> = ({ navItems }) => {
   };
 
   return (
-    <div className={classes.header} role="banner">
+    <div>
       <div className={classes.gradient} />
-      <Container>
-        <Row alignItems="center" className={classes.header__inner}>
-          {/* LOGO */}
-          <Col col={7} colMd={3} colLg={3}>
-            <Link to="/" label={'COVID PORTAL - ' + I18n('home')}>
-              <img src={headerLogo} alt="Covid Portal logo" />
-            </Link>
-          </Col>
-          {/* MOBILE TOGGLE */}
-          <Col
-            col={5}
-            className={classnames(
-              classes.nav__toggleWrapper,
-              'd-md-none text-right',
-            )}
-          >
-            {/* MENU */}
+
+      <div className={classes.header} role="banner">
+        <Container>
+          <Row alignItems="center" className={classes.header__inner}>
+            {/* LOGO */}
+            <Col col={7} colMd={3} colLg={3}>
+              <Link to="/" label={'COVID PORTAL - ' + I18n('home')}>
+                <img src={headerLogo} alt="Covid Portal logo" />
+              </Link>
+            </Col>
+            {/* MOBILE TOGGLE */}
+            <Col
+              col={5}
+              className={classnames(
+                classes.nav__toggleWrapper,
+                'd-md-none text-right',
+              )}
+            >
+              {/* MENU */}
+              <div
+                className={classnames(
+                  classes.nav__toggle,
+                  isOpen && classes['nav__toggle--open'],
+                )}
+                onClick={toggleOpen}
+              >
+                <span />
+                <span />
+                <div>
+                  {(isOpen
+                    ? I18n('menu_close')
+                    : I18n('menu_open')
+                  ).toUpperCase()}
+                </div>
+              </div>
+            </Col>
+            {/* DESKTOP NAV & SEARCH */}
+            <Col col={12} colMd={8} colLg={9}>
+              <Row alignItems="center">
+                {/* NAVIGATION */}
+                <Col col={12} colLg={8} className="d-none d-md-block">
+                  <div className={classnames(classes.navigation, 'navigation')}>
+                    <ul className={classnames('nav nav--primary')}>
+                      {navItems.map(({ label, to }) => (
+                        <li className={classnames('nav__item')} key={label}>
+                          <Link
+                            to={to}
+                            className={classnames(
+                              'nav__link',
+                              classes.nav__link,
+                            )}
+                            activeClassName="active"
+                            partiallyActive={to !== '/'}
+                          >
+                            {label}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </Col>
+                {/* SEARCH */}
+              </Row>
+            </Col>
+            {/* MOBILE NAV */}
             <div
               className={classnames(
-                classes.nav__toggle,
-                isOpen && classes['nav__toggle--open'],
+                classes.nav__mobile,
+                isOpen && classes['nav__mobile--open'],
+                'd-md-none',
               )}
-              onClick={toggleOpen}
             >
-              <span />
-              <span />
-              <div>
-                {(isOpen
-                  ? I18n('menu_close')
-                  : I18n('menu_open')
-                ).toUpperCase()}
-              </div>
-            </div>
-          </Col>
-          {/* DESKTOP NAV & SEARCH */}
-          <Col col={12} colMd={8} colLg={9}>
-            <Row alignItems="center">
-              {/* NAVIGATION */}
-              <Col col={12} colLg={8} className="d-none d-md-block">
-                <div className={classnames(classes.navigation, 'navigation')}>
-                  <ul className={classnames('nav nav--primary')}>
-                    {navItems.map(({ label, to }) => (
-                      <li className={classnames('nav__item')} key={label}>
-                        <Link
-                          to={to}
-                          className={classnames('nav__link', classes.nav__link)}
-                          activeClassName="active"
-                          partiallyActive={to !== '/'}
-                        >
-                          {label}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
+              <div
+                className="d-flex flex-column justify-content-between"
+                style={{ height: '100%' }}
+              >
+                <div>
+                  {navItems.map(({ label, to }) => (
+                    <Link
+                      onClick={onUseLink}
+                      to={to}
+                      key={label}
+                      className={classnames(
+                        classes.nav__mobileLink,
+                        'container',
+                      )}
+                      activeClassName={classes['nav__mobileLink--active']}
+                      partiallyActive={to !== '/'}
+                    >
+                      {label}
+                    </Link>
+                  ))}
                 </div>
-              </Col>
-              {/* SEARCH */}
-            </Row>
-          </Col>
-          {/* MOBILE NAV */}
-          <div
-            className={classnames(
-              classes.nav__mobile,
-              isOpen && classes['nav__mobile--open'],
-              'd-md-none',
-            )}
-          >
-            <div
-              className="d-flex flex-column justify-content-between"
-              style={{ height: '100%' }}
-            >
-              <div>
-                {navItems.map(({ label, to }) => (
-                  <Link
-                    onClick={onUseLink}
-                    to={to}
-                    key={label}
-                    className={classnames(classes.nav__mobileLink, 'container')}
-                    activeClassName={classes['nav__mobileLink--active']}
-                    partiallyActive={to !== '/'}
-                  >
-                    {label}
-                  </Link>
-                ))}
-              </div>
-              <div className="mt-auto">
-                {TRoute('/') !== '/' ? (
-                  <Link
-                    to="/"
-                    noTR
-                    onClick={onUseLink}
-                    className={classnames(classes.nav__mobileLink, 'container')}
-                  >
-                    Čeština
-                  </Link>
-                ) : (
-                  <Link
-                    to="/en/"
-                    noTR
-                    onClick={onUseLink}
-                    className={classnames(classes.nav__mobileLink, 'container')}
-                  >
-                    English
-                  </Link>
-                )}
+                <div className="mt-auto">
+                  {TRoute('/') !== '/' ? (
+                    <Link
+                      to="/"
+                      noTR
+                      onClick={onUseLink}
+                      className={classnames(
+                        classes.nav__mobileLink,
+                        'container',
+                      )}
+                    >
+                      Čeština
+                    </Link>
+                  ) : (
+                    <Link
+                      to="/en/"
+                      noTR
+                      onClick={onUseLink}
+                      className={classnames(
+                        classes.nav__mobileLink,
+                        'container',
+                      )}
+                    >
+                      English
+                    </Link>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-          {/* DESKTOP LOCALE SELECT */}
-          <HeaderLocaleSelect
-            activeLocale={activeLocale}
-            onLocaleChange={setLocale}
-          />
-        </Row>
-      </Container>
+            {/* DESKTOP LOCALE SELECT */}
+            <HeaderLocaleSelect
+              activeLocale={activeLocale}
+              onLocaleChange={setLocale}
+            />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
