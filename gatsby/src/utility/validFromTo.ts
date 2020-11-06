a0e537f1112fc37546288835965fc147f653978e
Haaxor1689 <betkomaros@gmail.com>
2020-11-06 02:31:03 +0100

    Removed todo

--- gatsby/src/utility/validFromTo.ts
+++ gatsby/src/utility/validFromTo.ts
@@ -1,6 +1,5 @@
 import I18n from '@/components/i18n';
 
-//  TODO: Localize
 const validFromTo = (validFrom: string, validTo: string) =>
   `${validFrom ? `${I18n('from')} ${validFrom} ` : ''}${
     validTo ? `${I18n('to')} ${validTo}` : ''
