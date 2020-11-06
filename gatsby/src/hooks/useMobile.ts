7595701bfcd4c281e45b2664dd6ef32dca3bbb6c
Martin Nuc <martin@nuc.cz>
2020-11-06 13:01:47 +0100

    Use smaller image for background on mobile

--- gatsby/src/hooks/useMobile.ts
+++ gatsby/src/hooks/useMobile.ts
@@ -1,10 +1,11 @@
-import { useTheme, useMediaQuery } from '@material-ui/core';
+import { useTheme, useMediaQuery, Options } from '@material-ui/core';
 
 const useMobile = (
   key: number | 'xs' | 'sm' | 'md' | 'lg' | 'xl' = 'sm',
+  options?: Options,
 ): boolean => {
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down(key));
+  const matches = useMediaQuery(theme.breakpoints.down(key), options);
   return matches;
 };
 
