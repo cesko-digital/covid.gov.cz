3fd1715708894d262af10076fa69b9c762f0fefa
David Veselý <dawe.vesely@gmail.com>
2020-11-06 02:16:18 +0100

    Gatsby: Translate LookinfForSomething

--- gatsby/src/components/looking-for-something/looking-for-something.tsx
+++ gatsby/src/components/looking-for-something/looking-for-something.tsx
@@ -1,50 +1,37 @@
 import React from 'react';
 import classes from './looking-for-something.module.scss';
 import { ArrowForwardIos, Phone } from '@material-ui/icons';
+import I18n from '@/components/i18n';
 
-const defaultValues = {
-  searchingHeader: (
-    <>
-      <strong>Stále</strong> hledáte?
-    </>
-  ),
-  searchingDescription:
-    'Nenašli jste odpověď v žádné kategorii, tématu ani častých otázkách?',
-  callHeader: (
-    <>
-      Volejte{' '}
-      <strong>
-        <a className={classes.phoneNumber} href="tel:1221">
-          1221
-        </a>
-      </strong>
-    </>
-  ),
-  callDescription: 'Pro urychlení si prosím připravte svůj konkrétní dotaz.',
-};
-
-type Props = Partial<typeof defaultValues>;
-
-const LookingForSomething = ({
-  searchingHeader = defaultValues.searchingHeader,
-  searchingDescription = defaultValues.searchingDescription,
-  callHeader = defaultValues.callHeader,
-  callDescription = defaultValues.callDescription,
-}: Props) => {
+const LookingForSomething = () => {
   return (
     <div className={classes.container}>
       <section className={classes.section}>
-        <header className={classes.header}>{searchingHeader}</header>
+        <header
+          className={classes.header}
+          dangerouslySetInnerHTML={{
+            __html: I18n('still_searching_title'),
+          }}
+        />
         <ArrowForwardIos />
-        <p className={classes.description}>{searchingDescription}</p>
+        <p className={classes.description}>
+          {I18n('still_searching_description')}
+        </p>
       </section>
 
       <hr className={classes.separator} />
 
       <section className={classes.section}>
         <Phone fontSize="large" className={classes.phoneIcon} />
-        <header className={classes.header}>{callHeader}</header>
-        <p className={classes.description}>{callDescription}</p>
+        <header className={classes.header}>
+          {I18n('call_title')}{' '}
+          <strong>
+            <a className={classes.phoneNumber} href="tel:1221">
+              1221
+            </a>
+          </strong>
+        </header>
+        <p className={classes.description}>{I18n('call_description')}</p>
       </section>
     </div>
   );
