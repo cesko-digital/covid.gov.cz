d5e332f691b4ae85223b50ac2726fe25ba0097b9
Rostislav Klein <rostislav.klein@pipedrive.com>
2020-11-07 23:55:47 +0100

    refactored CategoryItem so it supports desktop interactions as well

--- gatsby/src/components/category-item/category-item.module.scss.d.ts
+++ gatsby/src/components/category-item/category-item.module.scss.d.ts
@@ -1,3 +1,5 @@
 export const categoryItem: string;
+export const chevron: string;
+export const isActive: string;
 export const categoryItemIcon: string;
 export const categoryItemTitle: string;
--- gatsby/src/components/category-item/category-item.tsx
+++ gatsby/src/components/category-item/category-item.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { KeyboardArrowRight } from '@material-ui/icons';
 import Link from '@/components/link';
 import ContentIcon from '@/components/content-icon/content-icon';
@@ -6,17 +7,27 @@ import React, { FC } from 'react';
 import styles from './category-item.module.scss';
 
 type Props = {
+  id: string;
   name: string;
   path: string;
   iconCode: string;
+  isActive: boolean;
 };
 
-const CategoryItem: FC<Props> = ({ name, path, iconCode }) => {
+const CategoryItem: FC<Props> = ({ name, path, iconCode, isActive }) => {
+  console.log({ isActive });
   return (
-    <Link to={path} className={styles.categoryItem + ' py-1'}>
+    <Link
+      to={path}
+      className={classNames(styles.categoryItem, 'py-2', {
+        [styles.isActive]: isActive,
+      })}
+    >
       <ContentIcon className={styles.categoryItemIcon} code={iconCode} />
       <span className={styles.categoryItemTitle}>{name}</span>
-      <KeyboardArrowRight style={{ fontSize: 16 }} className="color-yellow" />
+      <div className={styles.chevron}>
+        <KeyboardArrowRight style={{ fontSize: 18 }} className="color-yellow" />
+      </div>
     </Link>
   );
 };
