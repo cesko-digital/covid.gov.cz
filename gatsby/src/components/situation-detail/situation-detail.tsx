57c8cc98cd2b698320be19003fa66e352c45ee10
Rostislav Klein <rostislav.klein@pipedrive.com>
2020-11-07 11:35:29 +0100

    separated SituationDetail and MeasureDetails as both use different data, created TopicDetail for the similiar parts

--- gatsby/src/components/situation-detail/situation-detail.tsx
+++ gatsby/src/components/situation-detail/situation-detail.tsx
@@ -4,128 +4,96 @@ import { Room, Event } from '@material-ui/icons';
 
 import Container from '@/components/container';
 import Link from '@/components/link';
-import Breadcrumb from '@/components/breadcrumb';
-import Headline from '@/components/headline';
 import Time from '@/components/time';
 import I18n from '@/components/i18n';
 
-import styles from './situation-detail.module.scss';
 import Accordion from '../accordion';
 import ContentBox from '../content-box';
-import { ISituation } from '../../../graphql-types';
-
-interface Region {
-  name: string;
-}
-
-interface Link {
-  uri: string;
-  title?: string;
-}
+import { ISituationDetailFragment } from 'graphql-types';
+import { graphql } from 'gatsby';
+import TopicDetail from '../topic-detail';
 
 interface IProps {
-  situation: ISituation;
-  type: string;
+  situation: ISituationDetailFragment;
 }
 
-const SituationDetail: React.FC<IProps> = ({ situation, type }) => {
+const SituationDetail: React.FC<IProps> = ({ situation }) => {
   return (
-    <div className={styles.situationDetail}>
-      <Container>
-        <div className="pt-1">
-          <Breadcrumb
-            items={[
-              { title: I18n('home'), url: '/' },
-              {
-                title: I18n(
-                  type === 'measure' ? 'current_measures' : 'life_situations',
-                ),
-                url: I18n(`slug_${type}s`),
-              },
-              {
-                title: situation.relationships?.situation_type?.name,
-                url: situation.relationships?.situation_type?.path?.alias,
-              },
-              situation.title,
-            ]}
-            variant="inverse"
-          />
-        </div>
-        <div className="mt-3">
-          <Headline>{situation.title}</Headline>
-        </div>
-        <article className="bg-white rounded p-2 pb-3 mb-1">
-          <div
-            dangerouslySetInnerHTML={{
-              __html: situation?.content?.processed,
-            }}
-          />
-
-          {situation.links?.length ? (
-            <div className="mt-2">
-              <h3 className="mb-1 color-blue-dark">{I18n('related')}</h3>
-              <div>
-                {situation.links.map((link, index) => (
-                  <div key={index}>
-                    <Link className="color-blue mb-1" to={link.uri}>
-                      {link.title}
-                    </Link>
-                  </div>
-                ))}
-              </div>
+    <>
+      <TopicDetail
+        breadcrumbItems={[
+          { title: I18n('home'), url: '/' },
+          {
+            title: I18n('life_situations'),
+            url: I18n(`slug_situations`),
+          },
+          {
+            title: situation.relationships?.situation_type?.name,
+            url: situation.relationships?.situation_type?.path?.alias,
+          },
+          situation.title,
+        ]}
+        title={situation.title}
+        processedContent={situation?.content?.processed}
+      >
+        {situation.links.length ? (
+          <div className="mt-2">
+            <h3 className="mb-1 color-blue-dark">{I18n('related')}</h3>
+            <div>
+              {situation.links.map((link, index) => (
+                <div key={index}>
+                  <Link className="color-blue mb-1" to={link.uri}>
+                    {link.title}
+                  </Link>
+                </div>
+              ))}
             </div>
-          ) : (
-            ''
-          )}
+          </div>
+        ) : (
+          ''
+        )}
 
-          {situation.relationships?.region.length ? (
-            <div className="mt-2">
-              <h3 className="mb-1 color-blue-dark">
-                {I18n('location_validity')}
-              </h3>
-              <div className="d-flex align-items-center color-blue mb-1">
-                <Room />
-                &nbsp;
-                <span className="text-uppercase font-weight-medium">
-                  {situation.relationships.region
-                    .map((item) => item.name)
-                    .join(', ')}
-                </span>
-              </div>
-            </div>
-          ) : (
-            ''
-          )}
-
-          {situation.valid_from || situation.valid_to ? (
-            <div className="d-flex align-items-center color-blue">
-              <Event />
+        {situation.relationships?.region.length ? (
+          <div className="mt-2">
+            <h3 className="mb-1 color-blue-dark">
+              {I18n('location_validity')}
+            </h3>
+            <div className="d-flex align-items-center color-blue mb-1">
+              <Room />
               &nbsp;
               <span className="text-uppercase font-weight-medium">
-                {situation.valid_from && (
-                  <Time
-                    datetime={situation.valid_from}
-                    prefix={`${I18n('from')} `}
-                  />
-                )}
-
-                {situation.valid_to && (
-                  <Time
-                    datetime={situation.valid_to}
-                    prefix={`${I18n('to')} `}
-                  />
-                )}
+                {situation.relationships.region
+                  .map((item) => item.name)
+                  .join(', ')}
               </span>
             </div>
-          ) : (
-            ''
-          )}
+          </div>
+        ) : (
+          ''
+        )}
 
-          {/* <p className="mt-2 text-muted font-italic">
-            Naposledy bylo toto téma aktualizováno {situation.changed}
-          </p> */}
-        </article>
+        {situation.valid_from || situation.valid_to ? (
+          <div className="d-flex align-items-center color-blue">
+            <Event />
+            &nbsp;
+            <span className="text-uppercase font-weight-medium">
+              {situation.valid_from && (
+                <Time
+                  datetime={situation.valid_from}
+                  prefix={`${I18n('from')} `}
+                />
+              )}
 
+              {situation.valid_to && (
+                <Time datetime={situation.valid_to} prefix={`${I18n('to')} `} />
+              )}
+            </span>
+          </div>
+        ) : (
+          ''
+        )}
+      </TopicDetail>
+      <Container>
         {situation.questions_answers?.length ? (
           // TODO: localize
           <ContentBox variant="blue" title={I18n('faq')} boldedTitleCount={2}>
@@ -140,8 +108,42 @@ const SituationDetail: React.FC<IProps> = ({ situation, type }) => {
           ''
         )}
       </Container>
-    </div>
+    </>
   );
 };
+
+export const query = graphql`
+  fragment SituationDetail on situation {
+    title
+    content {
+      processed
+    }
+    links {
+      uri
+      title
+    }
+    relationships {
+      region {
+        name
+      }
+      situation_type {
+        name
+        path {
+          alias
+        }
+      }
+    }
+    path {
+      alias
+    }
+    questions_answers {
+      question
+      value
+    }
+    changed
+    valid_from
+    valid_to
+  }
+`;
 
 export default SituationDetail;
