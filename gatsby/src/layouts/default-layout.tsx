8f070a55f55b776f30a6e969536b07999ce5e81c
Rostislav Klein <rostislav.klein@pipedrive.com>
2020-11-07 12:03:10 +0100

    sticky footer fix

--- gatsby/src/layouts/default-layout.tsx
+++ gatsby/src/layouts/default-layout.tsx
@@ -11,19 +11,20 @@ const DefaultLayout: React.FC = ({ children }) => {
   const backgroundImageStyle = useBackgroundImageStyle();
 
   return (
-    <div className={classnames('body__wrapper', styles.wrapper)}>
-      <div style={backgroundImageStyle} className={styles.bkgPhoto}>
-        <Header
-          navItems={[
-            { label: I18n('home'), to: '/' },
-            { label: I18n('life_situations'), to: I18n('slug_situations') },
-            { label: I18n('current_measures'), to: I18n('slug_measures') }, // TODO: přidat podmínku pouze pokud je na HP obsah
-          ]}
-        />
-        <main className={styles.main}>
-          <div className={styles.mainInner}>{children}</div>
-        </main>
-      </div>
+    <div
+      style={backgroundImageStyle}
+      className={classnames('body__wrapper', styles.wrapper, styles.bkgPhoto)}
+    >
+      <Header
+        navItems={[
+          { label: I18n('home'), to: '/' },
+          { label: I18n('life_situations'), to: I18n('slug_situations') },
+          { label: I18n('current_measures'), to: I18n('slug_measures') }, // TODO: přidat podmínku pouze pokud je na HP obsah
+        ]}
+      />
+      <main className={styles.main}>
+        <div className={styles.mainInner}>{children}</div>
+      </main>
       <Footer />
     </div>
   );
