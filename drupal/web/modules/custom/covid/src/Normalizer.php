0ee863a28c1f3f2c4b0d09a8ae35b35f89f5aef3
Miro Michalicka <miro.michalicka@madeit.digital>
2020-11-08 21:17:49 +0100

    Drupal: Replace regular spaces with non-breakable.

--- drupal/web/modules/custom/covid/src/Normalizer.php
+++ drupal/web/modules/custom/covid/src/Normalizer.php
@@ -7,13 +7,15 @@ namespace Drupal\covid;
 use Drupal\Core\TypedData\Plugin\DataType\StringData;
 use Drupal\link\Plugin\Field\FieldType\LinkItem;
 use Drupal\serialization\Normalizer\TypedDataNormalizer;
+use Drupal\text\Plugin\Field\FieldType\TextLongItem;
+use Drupal\text\TextProcessed;
 
 class Normalizer extends TypedDataNormalizer {
 
   public function normalize($object, $format = NULL, array $context = []) {
     $data = parent::normalize($object, $format, $context);
-
-    if ($object instanceof StringData && ($parent = $object->getParent()) instanceof LinkItem && $object->getString() === "") {
+    $parent = $object->getParent();
+    if ($object instanceof StringData && $parent instanceof LinkItem && $object->getString() === "") {
       $parsedUrl = parse_url($parent->get('uri')->getValue());
       $url = '';
       foreach (['host', 'path'] as $part) {
@@ -22,6 +24,12 @@ class Normalizer extends TypedDataNormalizer {
         }
       }
       return $url;
+    } elseif ($object instanceof TextProcessed && $parent instanceof TextLongItem && $object->getName() === 'processed') {
+      // Replace regular spaces.
+      $output = mb_ereg_replace('(?<![<-])\b(\w(?:\b|\.?))(<? )', '\\1;\\xA0', $object->getString());
+      // Replace html non-breakable spaces.
+      $output = str_replace("&nsbp;", "\xA0", $output);
+      return $output;
     }
 
     return $data;
