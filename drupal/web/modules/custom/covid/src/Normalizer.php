a9c74fece636905f95a3a629f3aeddfab2bac01d
Miro Michalicka <miro.michalicka@madeit.digital>
2020-11-08 23:38:49 +0100

    Drupal: Replace regular spaces with non-breakable.

--- drupal/web/modules/custom/covid/src/Normalizer.php
+++ drupal/web/modules/custom/covid/src/Normalizer.php
@@ -1,35 +1,46 @@
 <?php
 
-
 namespace Drupal\covid;
 
-
+use Drupal\Core\Field\Plugin\Field\FieldType\StringLongItem;
 use Drupal\Core\TypedData\Plugin\DataType\StringData;
 use Drupal\link\Plugin\Field\FieldType\LinkItem;
 use Drupal\serialization\Normalizer\TypedDataNormalizer;
 use Drupal\text\Plugin\Field\FieldType\TextLongItem;
 use Drupal\text\TextProcessed;
+use Psy\Util\Str;
 
+/**
+ * Class Normalizer.
+ *
+ * @package Drupal\covid
+ */
 class Normalizer extends TypedDataNormalizer {
 
+  /**
+   * {@inheritdoc}
+   */
   public function normalize($object, $format = NULL, array $context = []) {
     $data = parent::normalize($object, $format, $context);
     $parent = $object->getParent();
-    if ($object instanceof StringData && $parent instanceof LinkItem && $object->getString() === "") {
-      $parsedUrl = parse_url($parent->get('uri')->getValue());
-      $url = '';
-      foreach (['host', 'path'] as $part) {
-        if (isset($parsedUrl[$part])) {
-          $url .= $parsedUrl[$part];
+    if ( in_array(get_class($object), [StringData::class, StringLongItem::class])) {
+      if ($parent instanceof LinkItem && $object->getString() === "") {
+        $parsedUrl = parse_url($parent->get('uri')->getValue());
+        $url = '';
+        foreach (['host', 'path'] as $part) {
+          if (isset($parsedUrl[$part])) {
+            $url .= $parsedUrl[$part];
+          }
         }
+        return $url;
       }
-      return $url;
-    } elseif ($object instanceof TextProcessed && $parent instanceof TextLongItem && $object->getName() === 'processed') {
+      $space = html_entity_decode("&nbsp;");
+      return mb_ereg_replace('(?<![<-])\b(\w(?:\b|\.?))(<? )', "\\1$space", $object->getString());
+    }
+    elseif ($object instanceof TextProcessed && $parent instanceof TextLongItem && $object->getName() === 'processed') {
+      $space = html_entity_decode("&nbsp;");
       // Replace regular spaces.
-      $output = mb_ereg_replace('(?<![<-])\b(\w(?:\b|\.?))(<? )', '\\1;\\xA0', $object->getString());
-      // Replace html non-breakable spaces.
-      $output = str_replace("&nsbp;", "\xA0", $output);
-      return $output;
+      return mb_ereg_replace('(?<![<-])\b(\w(?:\b|\.?))(<? )', "\\1$space", $object->getString());
     }
 
     return $data;
