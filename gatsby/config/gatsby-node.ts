816389cfbe5b3fdba3d649e6066c244bdbbb6f4c
Filip Brebera <breberaf@gmail.com>
2020-11-08 05:21:02 +0100

    fix: why its not static

--- gatsby/config/gatsby-node.ts
+++ gatsby/config/gatsby-node.ts
@@ -1,19 +1,1 @@
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 export { createPages } from './create-pages';
-
-export const onCreateWebpackConfig = ({ stage, actions }) => {
-  return;
-
-  actions.setWebpackConfig({
-    plugins: [
-      new ForkTsCheckerWebpackPlugin({
-        eslint: {
-          enabled: true,
-          files: './src/**/*.{ts,tsx,js,jsx}',
-        },
-        async: false,
-        formatter: 'codeframe',
-      }),
-    ],
-  });
-};
