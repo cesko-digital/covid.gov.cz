02f3d573f7568e32d8ac482ce63f0a295d03784f
Rostislav Klein <rostislav.klein@pipedrive.com>
2020-11-10 23:02:57 +0100

    typescript check and stylelint fixes

--- gatsby/__tests__/alert.spec.tsx
+++ gatsby/__tests__/alert.spec.tsx
@@ -5,12 +5,12 @@ import { render } from '@testing-library/react';
 describe('alert.tsx', () => {
   const message = 'Testovací message';
   it('message renders correctly', () => {
-    const { getByText } = render(<Alert message={message} />);
+    const { getByText } = render(<Alert message={message} isInfo={false} />);
 
     expect(getByText(message)).toBeInTheDocument();
   });
   it('button is rendered', () => {
-    const { getByTestId } = render(<Alert message={message} link="#" />);
+    const { getByTestId } = render(<Alert message={message} link="#" isInfo />);
     expect(getByTestId('button-link')).toBeTruthy();
   });
 });
