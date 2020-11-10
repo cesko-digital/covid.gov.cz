6fe7001a287aaacce161431f33b343a8d7891b35
Marek Vantuch <marek.vantuch@gmail.com>
2020-11-10 13:18:02 +0100

    Drupal/feature/normalize (#307)
    * Drupal: Normalize links in content
    
    * Drupal: Move the normalization to a single class

--- drupal/web/modules/custom/covid/src/Normalizer/Normalizer.php
+++ drupal/web/modules/custom/covid/src/Normalizer/Normalizer.php
@@ -1,13 +1,11 @@
 <?php
 
-namespace Drupal\covid;
+namespace Drupal\covid\Normalizer;
 
 use Drupal\Core\Field\Plugin\Field\FieldType\StringLongItem;
 use Drupal\Core\TypedData\Plugin\DataType\StringData;
 use Drupal\link\Plugin\Field\FieldType\LinkItem;
 use Drupal\serialization\Normalizer\TypedDataNormalizer;
-use Drupal\text\Plugin\Field\FieldType\TextLongItem;
-use Drupal\text\TextProcessed;
 
 /**
  * Class Normalizer.
@@ -22,6 +20,7 @@ class Normalizer extends TypedDataNormalizer {
   public function normalize($object, $format = NULL, array $context = []) {
     $data = parent::normalize($object, $format, $context);
     $parent = $object->getParent();
+
     if ( in_array(get_class($object), [StringData::class, StringLongItem::class])) {
       if ($parent instanceof LinkItem && $object->getString() === "") {
         $parsedUrl = parse_url($parent->get('uri')->getValue());
@@ -34,11 +33,7 @@ class Normalizer extends TypedDataNormalizer {
         return $url;
       }
       $space = html_entity_decode("&nbsp;");
-      return mb_ereg_replace('(?<![<-])\b(\w(?:\b|\.?))(<? )', "\\1$space", $object->getString());
-    }
-    elseif ($object instanceof TextProcessed && $parent instanceof TextLongItem && $object->getName() === 'processed') {
-      $space = html_entity_decode("&nbsp;");
-      // Replace regular spaces.
+
       return mb_ereg_replace('(?<![<-])\b(\w(?:\b|\.?))(<? )', "\\1$space", $object->getString());
     }
 
