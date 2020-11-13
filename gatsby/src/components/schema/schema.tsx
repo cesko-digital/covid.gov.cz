d8da51a53f1a29b6312dd20b5f5ff95865ffd1c1
David Rychlý <davidrychly2@gmail.com>
2020-11-13 21:02:31 +0100

    fix: Gatsby: schema.org: eslint check error `body` not used in schema

--- gatsby/src/components/schema/schema.tsx
+++ gatsby/src/components/schema/schema.tsx
@@ -6,7 +6,6 @@ import { useTranslation } from '@/components/i18n';
 const BASE_URL = 'https://covid.gov.cz';
 
 interface IProps {
-  body?: string;
   langCode: string;
   datePublished?: string;
   dateModified?: string;
@@ -28,7 +27,6 @@ export const SchemaComp: React.FC<IProps> = ({
   isSpecialList,
   title,
   langCode,
-  body,
   isHomePage,
   breadcrumbItems,
 }) => {
