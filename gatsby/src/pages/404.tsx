b08b399b6e03598ed29475f92fbeb834f5ed3c35
Rostislav Klein <rostislav.klein@pipedrive.com>
2020-11-17 19:45:14 +0100

    translated 404 page + adjusted its style

--- gatsby/src/pages/404.tsx
+++ gatsby/src/pages/404.tsx
@@ -1,33 +1,39 @@
 import Button from '@/components/button';
 import Row from '@/components/row';
 import React from 'react';
-import { vh90 } from './404.module.scss';
+import styles from './404.module.scss';
 import Layout from '@/layouts/default-layout';
 import { Helmet } from 'react-helmet';
 import { ISitePageContext } from '@graphql-types';
+import { useTranslation } from '@/components/i18n';
+import Container from '@/components/container';
+import Col from '@/components/col';
 
 interface IProps {
   pageContext: ISitePageContext;
 }
 
-// todo: add localized title for button
 const PageNotFound: React.FC<IProps> = ({ pageContext }) => {
+  const { t } = useTranslation();
   return (
     <Layout pageContext={pageContext}>
-      <Helmet title="Page not found | COVID PORTAL" />
-      <Row
-        className={'bg-light flex-column p-5 mx-0 ' + vh90}
-        alignItems="center"
-        justify="center"
-      >
-        <h1 className="h2 font-weight-bold text-center mb-3">
-          Na této stránce bohužel nic není :(
-        </h1>
-        <h2 className="h3 font-weight-bold text-center mb-3">
-          Page Not Found 404
-        </h2>
-        <Button variant="contained" text="Zpět na úvodní stránku" href="/" />
-      </Row>
+      <Helmet title={`${t('page_not_found')} | COVID PORTAL`} />
+      <Container>
+        <Row className="flex-column mt-5" alignItems="center" justify="center">
+          <Col
+            col={12}
+            colSm={8}
+            colXl={6}
+            className={'bg-light text-center ' + styles.wrapper}
+          >
+            <h2 className="h3 mb-2 text-center">{t('page_not_found')}</h2>
+            <h1 className="h2 font-weight-bold text-center mb-3">
+              {t('not_found')} :(
+            </h1>
+            <Button variant="contained" text={t('back_to_home')} href="/" />
+          </Col>
+        </Row>
+      </Container>
     </Layout>
   );
 };
