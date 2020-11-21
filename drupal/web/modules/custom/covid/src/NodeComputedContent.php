9e247822acfd50360f98f8e976472ebc77fb8e05
Marek Vantuch <marek.vantuch@gmail.com>
2020-11-21 19:59:14 +0100

    Drupal: Measure version - calculate description

--- drupal/web/modules/custom/covid/src/NodeComputedContent.php
+++ drupal/web/modules/custom/covid/src/NodeComputedContent.php
@@ -7,7 +7,9 @@ namespace Drupal\covid;
 use Drupal;
 use Drupal\Core\Field\FieldItemList;
 use Drupal\Core\TypedData\ComputedItemListTrait;
+use Drupal\covid\Helper\MeasureHelper;
 use Drupal\node\NodeInterface;
+use Drupal\paragraphs\ParagraphInterface;
 
 class NodeComputedContent extends FieldItemList {
 
@@ -31,6 +33,8 @@ class NodeComputedContent extends FieldItemList {
 
       if ($node->bundle() === 'situation') {
         $value = $this->getSituationValue($node, $langcode);
+      } else if ($node->bundle() === 'measure') {
+        $value = $this->getMeasureValue($node, $langcode);
       } else if ($node->hasField('field_content')) {
         $value = $node->field_content->value;
       }
@@ -57,9 +61,7 @@ class NodeComputedContent extends FieldItemList {
     if ($pes) {
       foreach ($node->field_pes_content->referencedEntities() as $paragraph) {
         if ($paragraph->field_pes->entity->id() === $pes->id()) {
-          if ($paragraph->hasTranslation($langcode)) {
-            $paragraph = $paragraph->getTranslation($langcode);
-          }
+          $paragraph = $this->getTranslation($paragraph, $langcode);
 
           return $paragraph->field_content->value;
         }
@@ -67,6 +69,44 @@ class NodeComputedContent extends FieldItemList {
     }
 
     return $node->field_content->value;
+  }
+
+  /**
+   * Get the correct value of measure node depending on the current time.
+   *
+   * If no version is found, the default content is returned.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   * @param string $langcode
+   *
+   * @return mixed
+   */
+  private function getMeasureValue(NodeInterface $node, string $langcode) {
+    $version = MeasureHelper::getActiveVersion($node);
+
+    if ($version) {
+      $version = $this->getTranslation($version, $langcode);
+
+      return $version->field_content->value;
+    }
+
+    return $node->field_content->value;
+  }
+
+  /**
+   * Get the correct translation of a paragraph.
+   *
+   * @param \Drupal\paragraphs\ParagraphInterface $paragraph
+   * @param string $langcode
+   *
+   * @return \Drupal\paragraphs\ParagraphInterface
+   */
+  private function getTranslation(ParagraphInterface $paragraph, string $langcode): ParagraphInterface {
+    if ($paragraph->hasTranslation($langcode)) {
+      return $paragraph->getTranslation($langcode);
+    }
+
+    return $paragraph;
   }
 
 }
