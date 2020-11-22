0d8d28dcd1ff40917b72a26334c0b511b8a84acd
Miro Michalicka <miro.michalicka@gmail.com>
2020-11-22 16:56:42 +0100

    Drupal: Force users to agree with license agreement. (#395)

--- drupal/web/modules/custom/covid/src/EventSubscriber/RequestEventSubscriber.php
+++ drupal/web/modules/custom/covid/src/EventSubscriber/RequestEventSubscriber.php
@@ -8,6 +8,7 @@ use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\Url;
 use Drupal\user\Entity\User;
+use Drupal\user\UserDataInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
@@ -42,6 +43,13 @@ class RequestEventSubscriber implements EventSubscriberInterface {
   protected $messenger;
 
   /**
+   * User data.
+   *
+   * @var UserDataInterface
+   */
+  protected $userData;
+
+  /**
    * RequestEventSubscriber constructor.
    *
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
@@ -50,11 +58,14 @@ class RequestEventSubscriber implements EventSubscriberInterface {
    *   The current user.
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    *   The messenger.
+   * @param \Drupal\user\UserDataInterface $userData
+   *   User data.
    */
-  public function __construct(RouteMatchInterface $route_match, AccountInterface $current_user, MessengerInterface $messenger) {
+  public function __construct(RouteMatchInterface $route_match, AccountInterface $current_user, MessengerInterface $messenger, UserDataInterface $userData) {
     $this->routeMatch = $route_match;
     $this->currentUser = $current_user;
     $this->messenger = $messenger;
+    $this->userData = $userData;
   }
 
   /**
@@ -67,6 +78,7 @@ class RequestEventSubscriber implements EventSubscriberInterface {
     // Only for authenticated users.
     if ($this->currentUser->isAuthenticated()) {
       $user = User::load($this->currentUser->id());
+      $license_agreement = $this->userData->get('covid', $user->id(), 'license_agreement');
 
       // Check if TFA is disabled.
       if ($user->get('one_time_password')->isEmpty()) {
@@ -76,6 +88,7 @@ class RequestEventSubscriber implements EventSubscriberInterface {
           'entity.user.edit_form',
           'one_time_password.setup_form',
           'user.logout',
+          'covid.license_agreement',
         ])) {
 
           // Redirect to TFA setup form and show message.
@@ -83,8 +96,59 @@ class RequestEventSubscriber implements EventSubscriberInterface {
             'user' => $user->id(),
           ]);
           $event->setResponse(new RedirectResponse($url->toString()));
-          $this->messenger->addError($this->t('Please enable two-factor authentication.'));
+          $this->messenger->addError("Prosím zapněte dvoufaktorovou autorizaci pro váš účet.");
         }
+      }
+    }
+  }
+
+  /**
+   * Force user to agree with license.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   The request event.
+   */
+  public function forceLicenseAgreement(RequestEvent $event) {
+    // Only for authenticated users.
+    if ($this->currentUser->isAuthenticated()) {
+      $user = User::load($this->currentUser->id());
+      $license_agreement = $this->userData->get('covid', $user->id(), 'license_agreement');
+
+      // Check if TFA is disabled.
+      if (empty($license_agreement)) {
+        // Only if current route is not ignored.
+        if (!in_array($this->routeMatch->getRouteName(), [
+          'entity.user.edit_form',
+          'one_time_password.setup_form',
+          'user.logout',
+          'covid.license_agreement',
+        ])) {
+          // Redirect to TFA setup form and show message.
+          $url = new Url('covid.license_agreement');
+          $event->setResponse(new RedirectResponse($url->toString()));
+          $this->messenger->addError("Prosíme o udělení souhlasu s licenčním dojednáním.");
+        }
+      }
+    }
+  }
+
+  /**
+   * Don't allow access to license agreement page if user already agreed with it.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   The request event.
+   */
+  public function checkLicenseAgreement(RequestEvent $event) {
+    // Only for authenticated users.
+    if ($this->currentUser->isAuthenticated()) {
+      $user = User::load($this->currentUser->id());
+      $license_agreement = $this->userData->get('covid', $user->id(), 'license_agreement');
+
+      // Don't allow access to license agreement page if user already agreed.
+      if ($this->routeMatch->getRouteName() === 'covid.license_agreement' && !empty($license_agreement)) {
+        $url = new Url('<front>');
+        $event->setResponse(new RedirectResponse($url->toString()));
+        $this->messenger->addStatus("Souhlas s licenčním ujednáním  byl již udělen.");
       }
     }
   }
@@ -94,6 +158,8 @@ class RequestEventSubscriber implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents() {
     $events[KernelEvents::REQUEST][] = ['checkOneTimePasswordEnabled'];
+    $events[KernelEvents::REQUEST][] = ['forceLicenseAgreement'];
+    $events[KernelEvents::REQUEST][] = ['checkLicenseAgreement'];
     return $events;
   }
 
---
+++ drupal/web/modules/custom/covid/src/Form/LicenseAgreementForm.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Drupal\covid\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\user\UserDataInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a Covid form.
+ */
+class LicenseAgreementForm extends FormBase {
+
+  /**
+   * User Data.
+   *
+   * @var \Drupal\user\UserDataInterface
+   */
+  private $userData;
+
+  /**
+   * LicenseAgreementForm constructor.
+   *
+   * @param \Drupal\user\UserDataInterface $userData
+   *   User data.
+   */
+  public function __construct(UserDataInterface $userData) {
+    $this->userData = $userData;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('user.data')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'covid_license_agreement';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['text'] = [
+      '#markup' => <<<EOT
+<div class="content--narrow">
+<p>Dobrý den, obsah webu je zveřejňován pod veřejnou licencí
+Creative Commons CC BY 4.0 Mezinárodní, abychom ještě umocnili
+veřejně-prospěšný účel webu a umožnili všem zájemcům zveřejněné texty
+dále používat. CC BY je veřejnou licenci, která umožňuje komukoliv texty
+užívat (tedy zejména kopírovat, sdílet, rozvíjet atd.), pokud uvedou
+původ díla. Abychom toto mohli udělat, potřebujeme Váš souhlas se
+zveřejněním díla pod příslušnou licencí a s tím, že texty budou
+prezentovány pod jménem Národní agentury pro komunikačních a
+informační technologie (NAKIT), která celý projekt zaštiťuje a organizuje.</p>
+<p>Moc děkujeme za spolupráci. Váš souhlas pomůže k tomu, abychom potřebné
+informace dostali k širšímu publiku.</p>
+<p><strong>Potvrďte prosím zaškrtnutím políčka níže, že souhlasíte s následujícími
+licenčními ujednáními:</strong></p>
+<p><i>„Vstupem do [editorského rozhraní] uděluji Národní agentuře pro komunikačních
+a informační technologie (dále jen „NAKIT“) bezúplatné nevýhradní oprávnění k
+užití všech literárních děl, které jsem vytvořila/vytvořil nebo vytvořím pro
+webovou stránku Covid portál (dále jen „díla“), a to ke všem způsobům užití
+těchto děl, územně, věcně i množstevně neomezeným, s neomezenými právy
+provádět na díle změny, úpravy, spojení s jiným dílem či díly, vytváření
+odvozených děl, a to na celou dobu trvání majetkových práv, a to včetně práva
+NAKIT zveřejnit dílo pod veřejnou licencí Creative Commons CC BY 4.0 M
+ezinárodní. Uděluji NAKIT rovněž souhlas s tím, že smí díla zveřejnit pod
+svým jménem. NAKIT není povinen dílo užít. Zároveň prohlašuji, že na
+dílech neváznou práva třetích osob.“</i></p></div>
+EOT,
+    ];
+
+    $form['agreement'] = [
+      '#type' => 'checkbox',
+      '#title' => 'Souhlasím',
+      '#required' => TRUE,
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Save time of submission.
+    $this->userData->set('covid', $this->currentUser()
+      ->id(), 'license_agreement', time());
+    $form_state->setRedirect('<front>');
+  }
+
+}
