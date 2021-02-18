3fbd38ee0700eebe067b7f4024440205c960bdbb
vitkolos <vit.kolos@gmail.com>
2021-02-18 12:12:33 +0100

    refactor: czech-days

--- gatsby/src/components/czech-days/czech-days.tsx
+++ gatsby/src/components/czech-days/czech-days.tsx
@@ -1,22 +1,22 @@
 import { useCurrentLanguage } from '@/components/i18n';
 
 const CZECH_DAYS_FROM_TO = [
+  'neděle ',
   'pondělí ',
   'úterý ',
   'středy ',
   'čtvrtka ',
   'pátku ',
   'soboty ',
-  'neděle ',
 ] as const;
 const CZECH_DAYS_ON = [
+  'neděli ',
   'pondělí ',
   'úterý ',
   'středu ',
   'čtvrtek ',
   'pátek ',
   'sobotu ',
-  'neděli ',
 ] as const;
 
 export type CzechDaysFromToKey = typeof CZECH_DAYS_FROM_TO[number];
@@ -30,34 +30,8 @@ export const useCurrentCzechDay = (
   const currentLanguage = useCurrentLanguage();
 
   if (currentLanguage === 'cs' && isShownShorterDate === false) {
-    if (usedForLastUpdatedCase === false) {
-      if (datetime === 0) {
-        return CZECH_DAYS_FROM_TO[6];
-      } else if (datetime === 1) {
-        return CZECH_DAYS_FROM_TO[0];
-      } else if (datetime === 2) {
-        return CZECH_DAYS_FROM_TO[1];
-      } else if (datetime === 3) {
-        return CZECH_DAYS_FROM_TO[2];
-      } else if (datetime === 4) {
-        return CZECH_DAYS_FROM_TO[3];
-      } else if (datetime === 5) {
-        return CZECH_DAYS_FROM_TO[4];
-      } else return CZECH_DAYS_FROM_TO[5];
-    } else {
-      if (datetime === 0) {
-        return CZECH_DAYS_ON[6];
-      } else if (datetime === 1) {
-        return CZECH_DAYS_ON[0];
-      } else if (datetime === 2) {
-        return CZECH_DAYS_ON[1];
-      } else if (datetime === 3) {
-        return CZECH_DAYS_ON[2];
-      } else if (datetime === 4) {
-        return CZECH_DAYS_ON[3];
-      } else if (datetime === 5) {
-        return CZECH_DAYS_ON[4];
-      } else return CZECH_DAYS_ON[5];
-    }
+    return usedForLastUpdatedCase === false
+      ? CZECH_DAYS_FROM_TO[datetime]
+      : CZECH_DAYS_ON[datetime];
   }
 };
