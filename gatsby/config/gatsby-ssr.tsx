de6a82f3aad8240822154c5ea54c92bdd7885dc9
David Rychlý <davidrychly2@gmail.com>
2021-02-21 13:42:46 +0100

    Gatsby/feat: SSR HTML lang fix

--- gatsby/config/gatsby-ssr.tsx
+++ gatsby/config/gatsby-ssr.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { GatsbySSR } from 'gatsby';
 import RootLayout from '../src/components/root-layout';
+import { isLanguageSupported } from '../src/components/i18n/TranslatorContext';
 
 export const onRenderBody: GatsbySSR['onRenderBody'] = ({
+  pathname,
   setHtmlAttributes,
 }) => {
+  let language = 'cs';
+  const matchedLanguage = pathname.match(/\/([a-z]{2})\/.*/);
+
+  if (matchedLanguage) {
+    const [, langCode] = matchedLanguage;
+    if (isLanguageSupported(langCode)) {
+      language = langCode;
+    }
+  }
+
   setHtmlAttributes({
     className: 'pvs-theme',
-    lang: 'cs',
+    lang: language,
   });
 };
 
